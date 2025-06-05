d07c79aab3f276848d3a0ad3071f5216b4eb3cbe
Sheikh Aamir <113338901+aamirk0008@users.noreply.github.com>
2025-06-05 17:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	const spaceIndex = str.indexOf(' ');
+	
+
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
