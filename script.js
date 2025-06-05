ef9d5756edd69c2b758fb65ca7e2114f8986f2e6
Sheikh Aamir <113338901+aamirk0008@users.noreply.github.com>
2025-06-05 17:41:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function firstWord(s) {
   // your code here
- if (str === '') {
+	if (s === '') {
         return '';
     }
     
     // Skip leading spaces
     let start = 0;
-    while (start < str.length && str[start] === ' ') {
+    while (start < s.length && s[start] === ' ') {
         start++;
     }
     
     // If string is all spaces, return empty string
-    if (start === str.length) {
+    if (start === s.length) {
         return '';
     }
     
     // Find the end of the first word (next space or end of string)
     let end = start;
-    while (end < str.length && str[end] !== ' ') {
+    while (end < s.length && s[end] !== ' ') {
         end++;
     }
     
     // Return the first word
-    return str.substring(start, end);
+    return s.substring(start, end);
 }
 
 // Do not change the code below
